f0a1463941461b1e27ec92facd2a28c8d00871ed
aldeig <alkin.denis@gmail.com>
2024-05-23 17:53:43 +0400

    feat: added MappedAsDataclass

--- app/src/services/db/base.py
+++ app/src/services/db/base.py
@@ -1,12 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
 from app.settings import settings
 
 
-class Base(AsyncAttrs, DeclarativeBase):
+class Base(AsyncAttrs, MappedAsDataclass, DeclarativeBase):
     pass
 
 
-engine = create_async_engine(settings.DATABASE_URL)
+if not settings.SQLITE_DSN:
+    raise ValueError("SQLITE_DSN not set")
+engine = create_async_engine(settings.SQLITE_DSN)
 session_factory = async_sessionmaker(engine, expire_on_commit=False)
