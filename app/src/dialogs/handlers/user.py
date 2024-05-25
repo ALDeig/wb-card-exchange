e1b07115a7d705caf016776ad632eeb9b75ca86d
aldeig <alkin.denis@gmail.com>
2024-05-25 13:20:26 +0400

    fix: send start message

--- app/src/dialogs/handlers/user.py
+++ app/src/dialogs/handlers/user.py
@@ -8,7 +8,7 @@ from app.src.dialogs.states import PostDetailsState
 from app.src.services.data_collection import texts
 
 # from app.src.services.data_collection.collection import prepare_post
-from app.src.services.user import save_user
+from app.src.services.user import get_start_text, save_user
 
 router = Router()
 
@@ -16,8 +16,10 @@ router = Router()
 @router.message(Command("start"), flags={"db": True})
 async def cmd_start(msg: Message, db: AsyncSession, state: FSMContext):
     await save_user(db, msg.chat.id, msg.chat.full_name, msg.chat.username)
+    start_text = await get_start_text(db)
     # photo, text = await prepare_post(db)
     # await msg.answer_photo(photo, caption=text)
     # return
+    await msg.answer(start_text)
     await msg.answer(texts.CATEGORY)
     await state.set_state(PostDetailsState.category)
--- app/src/services/user.py
+++ app/src/services/user.py
@@ -1,11 +1,19 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.services.db.dao.user_dao import UserDao
+from app.src.services.db.dao.service_message_dao import ServiceMessageDao
 
 
 async def save_user(
     session: AsyncSession, user_id: int, full_name: str, username: str | None
-):
+) -> None:
     await UserDao(session).insert_or_nothing(
         "id", id=user_id, full_name=full_name, username=username
     )
+
+
+async def get_start_text(session: AsyncSession) -> str:
+    text = await ServiceMessageDao(session).find_one_or_none(key="start")
+    if text is None:
+        return "Приветствие"
+    return text.value
