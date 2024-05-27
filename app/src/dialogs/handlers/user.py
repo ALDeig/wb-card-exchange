dab56fab67bd2814e8be2e33f685c02993335722
aldeig <alkin.denis@gmail.com>
2024-05-27 09:16:54 +0400

    refactor: команда старт очищает стейт и присылает кнопку создания нового лота

--- app/src/dialogs/handlers/user.py
+++ app/src/dialogs/handlers/user.py
@@ -4,10 +4,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.src.dialogs.states import PostDetailsState
-from app.src.services.data_collection import texts
-
-# from app.src.services.data_collection.collection import prepare_post
+from app.src.dialogs.keyboards.collection import kb_new_lot
 from app.src.services.user import get_start_text, save_user
 
 router = Router()
@@ -15,11 +12,7 @@ router = Router()
 
 @router.message(Command("start"), flags={"db": True})
 async def cmd_start(msg: Message, db: AsyncSession, state: FSMContext):
+    await state.clear()
     await save_user(db, msg.chat.id, msg.chat.full_name, msg.chat.username)
     start_text = await get_start_text(db)
-    # photo, text = await prepare_post(db)
-    # await msg.answer_photo(photo, caption=text)
-    # return
-    await msg.answer(start_text)
-    await msg.answer(texts.CATEGORY)
-    await state.set_state(PostDetailsState.category)
+    await msg.answer(start_text, reply_markup=kb_new_lot)
